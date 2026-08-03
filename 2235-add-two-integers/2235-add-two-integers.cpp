148e33f91a358d0926a989f5ceffd19b31f1026c
Shubham Rai <shubhamrai7233@gmail.com>
2026-08-03 19:30:51 +0530

    Time: 0 ms (100%), Space: 8 MB (69.37%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        int sum = (num1,num2);
-        sum = num1 + num2;
-         return sum;
+        int sum = num1 + num2;
+        return sum;
     }
 };
