36c5f006f9dc25523d1c4c7b83a617518aed8b11
Shubham Rai <shubhamrai7233@gmail.com>
2026-08-23 13:23:15 +0530

    Time: 0 ms (100%), Space: 11.8 MB (94.88%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> record;
+
+        for (string op : operations) {
+
+            if (op == "C") {
+                record.pop_back();
+            }
+
+            else if (op == "D") {
+                record.push_back(2 * record.back());
+            }
+
+            else if (op == "+") {
+                int n = record.size();
+                record.push_back(record[n - 1] + record[n - 2]);
+            }
+
+            else {
+                record.push_back(stoi(op));
+            }
+        }
+
+        int sum = 0;
+
+        for (int score : record) {
+            sum += score;
+        }
+
+        return sum;
+    }
+};
