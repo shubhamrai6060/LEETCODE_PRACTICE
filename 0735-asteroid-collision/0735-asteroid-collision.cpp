2574c1886ef8a2683cc33b4f3695ae7096e1f5a1
Shubham Rai <shubhamrai7233@gmail.com>
2026-08-24 14:29:18 +0530

    Time: 0 ms (100%), Space: 21.6 MB (89.95%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        vector<int> st;
+
+        for (int asteroid : asteroids) {
+
+            bool destroyed = false;
+
+            while (!st.empty() && st.back() > 0 && asteroid < 0) {
+
+                if (st.back() < -asteroid) {
+                    st.pop_back();
+                    continue;
+                }
+
+                else if (st.back() == -asteroid) {
+                    st.pop_back();
+                }
+
+                destroyed = true;
+                break;
+            }
+
+            if (!destroyed) {
+                st.push_back(asteroid);
+            }
+        }
+
+        return st;
+    }
+};
